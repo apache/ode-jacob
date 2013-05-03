ad50030c9e5fafc1602d77b23a9fa6bb85649541
Tammo van Lessen <tvanlessen@gmail.com>
2013-05-03 21:45:19 +0200

    capital J? no!

--- src/test/java/org/apache/ode/jacob/soup/Jackson/JacksonSoupTest.java
+++ src/test/java/org/apache/ode/jacob/soup/Jackson/JacksonSoupTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.ode.jacob.soup.Jackson;
+package org.apache.ode.jacob.soup.jackson;
 
 import java.util.ArrayList;
 import java.util.List;
